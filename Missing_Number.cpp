444850a292bc901bc993319f0d567aaaebb31f7e
abdulqadir18 <boxwalaabdulqadir2002@gmail.com>
2021-11-06 13:10:01 +0530

    6nov

---
+++ Missing_Number.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+int n;
+cin>>n;
+vector <int> a(n-1);
+for(int i=0; i<n-1; i++)
+{
+  cin>>a[i];
+}
+
+sort(a.begin(),a.end());
+for(int i=0; i<n-1; i++)
+{
+  if(a[i]!=i+1)
+  {
+    cout<<i+1;
+    return 0;
+  }
+}
+
+cout<<n;
+
+return 0;
+}
